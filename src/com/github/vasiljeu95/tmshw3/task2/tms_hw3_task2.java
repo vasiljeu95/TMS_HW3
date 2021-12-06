95e9df27b285db516354535e8fe3245284220936
vasiljeu95 <vasilyeu95@gmail.com>
2021-12-06 23:54:45 +0300

    task2commit

--- src/com/github/vasiljeu95/tmshw3/task2/tms_hw3_task2.java
+++ src/com/github/vasiljeu95/tmshw3/task2/tms_hw3_task2.java
@@ -1,5 +1,15 @@
 package com.github.vasiljeu95.tmshw3.task2;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class tms_hw3_task2 {
+    public static void main(String[] args) {
+        int[] arr = new int [4];
 
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+5;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
 }
